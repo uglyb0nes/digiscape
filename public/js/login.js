625e517307069734f0e1c26f55f676dbe7937229
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-08-28 10:27:02 -0700

    login function

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+async function loginHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dash');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login').addEventListener('submit', loginHandler);
